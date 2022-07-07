86b920ce4aa8c398dd38ffcf7ed800c83f78308a
Lucas Scarano <scaranolucas75@gmail.com>
2022-07-07 14:48:54 -0300

    Agregados los rectangulos y deteccion del dedo indice

--- handsDetection.py
+++ handsDetection.py
@@ -5,19 +5,29 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+colors = {
+    'blue': (255, 0, 0),
+    'green': (0, 255, 0),
+    'red': (0, 0, 255)
+}
+rect1 = (10, 10)
+rect2 = (200, 200)
+
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
         model_complexity=0,
+        max_num_hands=4,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, image = cap.read()
+        image = cv2.flip(image, 1)
         if not success:
             print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
             continue
-
+        h, w, _ = image.shape
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
@@ -35,11 +45,19 @@ with mp_hands.Hands(
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
-        # Flip the image horizontally for a selfie-view display.
-        image_flipped = cv2.flip(image, 1)
-        cv2.rectangle(image_flipped, (10, 10), (200, 200), (255, 0, 0), 2)
-        cv2.imshow('MediaPipe Hands', image_flipped)
 
-        if cv2.waitKey(5) & 0xFF == 27:
+            if rect1[0] <= results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * w <= \
+                rect2[0] and rect1[1] <= results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * h <= \
+                    rect2[1]:
+                cv2.rectangle(image, rect1, rect2, colors['green'], 2)
+            else:
+                cv2.rectangle(image, rect1, rect2, colors['red'], 2)
+        else:
+            cv2.rectangle(image, rect1, rect2, colors['blue'], 2)
+
+        cv2.imshow('MediaPipe Hands', image)
+
+        # Salir con Esc o 'q'
+        if cv2.waitKey(5) & 0xFF == 27 or cv2.waitKey(5) & 0xFF == ord('q'):
             break
 cap.release()
